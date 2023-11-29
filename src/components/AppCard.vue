352c9d71a9f777bf31f8c4325019c938ae3cee2e
FrancescoMazzocchi <123@email.it>
2023-11-29 17:24:15 +0100

    add features: components + style

---
+++ src/components/AppCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props:
+        ["products"]
+}
+</script>
+
+<template>
+    <div class="row">
+        <div class="col-2" id="card" v-for="prodotto in products">
+            <img :src="prodotto.thumb" alt="">
+            <p class="text-uppercase">{{ prodotto.series }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+img {
+    width: 100%;
+    aspect-ratio: 1;
+    margin-bottom: 1rem;
+}
+
+#card {}
+</style>
